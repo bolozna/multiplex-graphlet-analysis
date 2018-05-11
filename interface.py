8bb0c43e759a7a152570979adc7f3d29f152741f
Tarmo Nurmi <tarmo.nurmi@aalto.fi>
2018-05-11 14:22:49 +0300

    fixed bug where network couplings were not communicated to invariant creation

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import pymnet
 import os
 import itertools
+#import pipeline
 import pipeline
 
 def graphlet_degree_distributions(network,nnodes,nlayers,allowed_aspects='all',save_name=None):
@@ -18,7 +19,7 @@ def graphlet_degree_distributions(network,nnodes,nlayers,allowed_aspects='all',s
     net_nlayers = len(net_layers)
     net_nodes = list(network.iter_nodes())
     
-    graphlets,invs = pymnet.graphlets.graphlets(nnodes,net_layers,nlayers,allowed_aspects=allowed_aspects)
+    graphlets,invs = pymnet.graphlets.graphlets(nnodes,net_layers,nlayers,couplings=network.couplings[0][0],allowed_aspects=allowed_aspects)
     auts = pymnet.graphlets.automorphism_orbits(graphlets,allowed_aspects=allowed_aspects)
     orbit_is = pymnet.graphlets.orbit_numbers(nnodes,graphlets,auts)
     orbit_list = pymnet.graphlets.ordered_orbit_list(orbit_is)
@@ -43,4 +44,3 @@ def graphlet_degree_distributions(network,nnodes,nlayers,allowed_aspects='all',s
             pipeline.write_orbit_counts(orbits,f_name,net_nodes,orbit_list)
         
     return
-    
