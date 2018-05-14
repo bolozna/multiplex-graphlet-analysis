c6799c463e00b71711de7f8f436b7a6d3fc5cd13
Tarmo Nurmi <tarmo.nurmi@aalto.fi>
2018-05-14 14:39:24 +0300

    added test for equivalence of orbit loading functions (something's causing differences in PR curves for different loaders)

--- tests.py
+++ tests.py
@@ -36,6 +36,50 @@ def test_example_network_interface_equivalence():
     print('The number of successes is '+str(successes))
     return total,successes
     
+def test_orbit_count_sum_functions():
+    '''
+    Loads all example results and checks that they sum to equivalent data frames.
+    Checks all three loading functions.
+    '''
+    # what's causing the difference in PR curves?
+    directory = 'Results/'
+    allowed_aspects = 'all'
+    
+    total = 0
+    successes = 0
+    for name in ['ba_plex','er_0','er_20']:
+        for network_num in range(0,5):
+            for nlayers in [1,2,3]:
+                fullname = name+'_'+str(network_num)
+                folder = directory+fullname+'_'+str(nlayers)
+                
+                if nlayers == 1 or nlayers == 2:
+                    n = 4
+                else:
+                    n = 3
+                    
+                if nlayers == 1:
+                    net_layers = [0]
+                else:
+                    net_layers = [0,1,2]
+                
+                graphlets,invs = pymnet.graphlets.graphlets(n,net_layers,nlayers,allowed_aspects=allowed_aspects)
+                auts = pymnet.graphlets.automorphism_orbits(graphlets,allowed_aspects=allowed_aspects)
+                orbit_is = pymnet.graphlets.orbit_numbers(n,graphlets,auts)
+                orbit_list = pymnet.graphlets.ordered_orbit_list(orbit_is)
+                
+                df1 = data_analysis.sum_orbit_counts(folder,orbit_list)
+                df2 = data_analysis.sum_orbit_counts_v2(folder,orbit_list)
+                df3 = interface.read_graphlet_degree_distribution_folder(folder)
+                
+                total += 1
+                if df1.equals(df2) and df2.equals(df3) and df3.equals(df1):
+                    successes += 1
+                
+    print('The number of networks is '+str(total))
+    print('The number of successes is '+str(successes))
+    return total,successes
+    
 def test_nonnumeric_layer_labels():
     # not finished
     net = pymnet.MultiplexNetwork(couplings='categorical')
